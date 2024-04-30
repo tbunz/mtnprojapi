51b4b948d203d497f30b2f7c6c200d9d46d3bd55
Trevor McKinney <trevmickey@gmail.com>
2024-04-30 16:45:09 -0600

    Kinda went down the rabbit hole today... trying to render JS after getting page HTML. Probably a better way (selenium?)

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
 
+import requests
+import urllib.parse
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/search/<query>")
+def search(query):
+    url_q = urllib.parse.quote(query)
+    full_url = "https://www.mountainproject.com/search?q=" + url_q
+
+    response = requests.get(full_url)
+    status = response.status_code
+    content = response.content
+
+    return {
+        "response": content
+    }
