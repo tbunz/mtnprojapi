e50d04b769a86f305d1e9c25147af2303a975652
Trevor <trevmickey@gmail.com>
2024-05-01 19:28:13 +0000

    Oh my god it actually worked I'm going to cry

--- app.py
+++ app.py
@@ -1,38 +1,25 @@
 from flask import Flask
-
-import urllib.parse
-import time 
-
-from selenium import webdriver 
-from selenium.webdriver import Chrome 
-from selenium.webdriver.common.by import By 
-
 app = Flask(__name__)
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @app.route("/search/<query>")
 def search(query):
 
-    options = webdriver.ChromeOptions() 
-    options.add_argument("--headless") # Set the Chrome webdriver to run in headless mode for scalability
+	chrome_options = Options()
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	driver = webdriver.Chrome(options=chrome_options)
 
-    # By default, Selenium waits for all resources to download before taking actions.
-    # However, we don't need it as the page is populated with dynamically generated JavaScript code.
-    options.page_load_strategy = "none"
+	driver.get("https://www.google.com")
 
-    # Pass the defined options objects to initialize the web driver 
-    driver = Chrome(options=options) 
-    # Set an implicit wait of 5 seconds to allow time for elements to appear before throwing an exception
-    driver.implicitly_wait(5)
+	content = {"hi": "hey"}
 
+	driver.quit()
 
+	return {"response": content}
 
-    # url_q = urllib.parse.quote(query)
-    # full_url = "https://www.mountainproject.com/search?q=" + url_q
-
-    # response = driver.get(full_url)
-
-    
-   
-    return {
-        "response": content
-    }
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
